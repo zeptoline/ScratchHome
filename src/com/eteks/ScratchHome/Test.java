9323d84954453e70599b16c713922b4253958ddb
Cédric Berland <zeptoline2@gmail.com>
2016-01-20 16:30:37 +0100

    First socket use

--- src/com/eteks/ScratchHome/Test.java
+++ src/com/eteks/ScratchHome/Test.java
@@ -1,5 +1,12 @@
 package com.eteks.ScratchHome;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 import com.eteks.sweethome3d.model.Home;
 import com.eteks.sweethome3d.model.HomePieceOfFurniture;
 
@@ -11,36 +18,93 @@ public class Test implements Runnable{
 	public Test(Home home) {
 		this.home = home;
 	}
-	
-	
+
+
 	public void terminate() {
 		running = false;
 	}
 
 	public void run() {
-		
+
 		running = true;
-		
-		while(running) {
-			try {
-				
-				Thread.sleep(5000);
-				changeColor(home);
-				
-			} catch (InterruptedException e) {
-				
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				
+
+		ServerSocket server = null;
+		String line;
+		BufferedReader d;
+		PrintStream os;
+
+		Socket clientSocket = null;
+
+		try 
+		{
+			server = new ServerSocket(2016);
+		}
+		catch (IOException e) 
+		{
+			System.out.println(e);
+		}   
+
+
+		try {
+			clientSocket = server.accept();
+			d = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+			os = new PrintStream(clientSocket.getOutputStream());
+
+			for (HomePieceOfFurniture fourniture : home.getFurniture()) {
+				os.println(String.valueOf(fourniture.hashCode()));
 			}
+			os.println("QUIT");
+
+			boolean choseFurniture = true;
+			int hash = 0;
+			int color = 0;
+			while(running) {
+				
+				line = d.readLine();
+				
+				
+				if(choseFurniture) {
+					hash = Integer.valueOf(line);
+					choseFurniture = false;
+				} else {
+					if (line.equals("red"))
+						color =  -65536;
+					else if (line.equals("blue"))
+						color = -16776961;
+					else if (line.equals("green"))
+						color =  -16711936;
+					else if (line.equals("black"))
+						color = -16777216;
+					else if (line.equals("white"))
+						color = -1;
+					else if (line.equals("gray"))
+						color =  -7829368;
+					else if (line.equals("yellow"))
+						color =-256 ;
+					else
+						color = 0;
+
+					if(hash != 0 && color != 0) {
+						changeColor(hash, color, home);
+					}
+					choseFurniture = true;
+
+				}
+				System.out.println(line); 
+
+			}
+		}   
+		catch (IOException e) {
+			System.out.println(e);
 		}
 	}
-	
-	  public void changeColor(Home home) {
-		  for (HomePieceOfFurniture fourniture : home.getFurniture()) {
-			fourniture.setColor(-65536);
+
+	public void changeColor(int hash, int color, Home home) {
+		for (HomePieceOfFurniture fourniture : home.getFurniture()) {
+			if(fourniture.hashCode() == hash)
+				fourniture.setColor(color);
 		}
-		  
-	  }
-	
+	}
+
 }
